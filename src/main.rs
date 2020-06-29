69bf68c417016415d510b63f07af784e2056dca2
Keith <keithdenning1@gmail.com>
2020-06-29 19:43:44 -0400

    Initital commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+extern crate glfw;
+
+extern crate gl;
+
+fn main() {
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+}
