57e68a96b802ec3ce541e790c8ef1980e747d655
Keith Denning <kdenning@captechventures.com>
2020-07-02 14:36:11 -0400

    Render triangle

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,169 @@
 extern crate glfw;
+use self::glfw::{Context, Key, Action};
 
 extern crate gl;
+use self::gl::types::*;
+
+use std::sync::mpsc::Receiver;
+use std::ffi::CString;
+use std::ptr;
+use std::str;
+use std::mem;
+use std::os::raw::c_void;
+
+const VERTEX_SHADER_SOURCE: &str = r#"
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    void main() {
+        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }
+"#;
+
+const FRAGMENT_SHADER_SOURCE: &str = r#"
+    #version 330 core
+    out vec4 FragColor;
+    void main() {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    }
+"#;
 
 fn main() {
+    // glfw: initialize and configure
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
     glfw.window_hint(glfw::WindowHint::OpenGlProfile(
         glfw::OpenGlProfileHint::Core,
     ));
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+
+    // glfw window creation
+    let (mut window, events) = glfw.create_window(
+        800, 600, "LearnOpenGL", glfw::WindowMode::Windowed
+    ).expect("Failed to create GLFW window");
+
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    // gl: load all OpenGL function pointers
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    let (shader_program, vao, vbo) = unsafe {
+        // build and compile shader program
+        // vertex shader
+        let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
+        let c_str_vert = CString::new(VERTEX_SHADER_SOURCE.as_bytes()).unwrap();
+        gl::ShaderSource(vertex_shader, 1, &c_str_vert.as_ptr(), ptr::null());
+        gl::CompileShader(vertex_shader);
+
+        // check for shader compile errors
+        let mut success = gl::FALSE as GLint;
+        let mut info_log = Vec::with_capacity(512);
+        let info_log_len_ptr = &mut 0 as *mut GLsizei;
+        gl::GetShaderiv(vertex_shader, gl::COMPILE_STATUS, &mut success);
+        if success != gl::TRUE as GLint {
+            gl::GetShaderInfoLog(vertex_shader, 512, info_log_len_ptr, info_log.as_mut_ptr() as *mut GLchar);
+            info_log.set_len(*info_log_len_ptr as usize);
+            println!("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n{}", str::from_utf8(&info_log).unwrap());
+        }
+
+        // fragment shader
+        let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+        let c_str_frag = CString::new(FRAGMENT_SHADER_SOURCE.as_bytes()).unwrap();
+        gl::ShaderSource(fragment_shader, 1, &c_str_frag.as_ptr(), ptr::null());
+        gl::CompileShader(fragment_shader);
+
+        // check for shader compile errors
+        gl::GetShaderiv(fragment_shader, gl::COMPILE_STATUS, &mut success);
+        if success != gl::TRUE as GLint {
+            gl::GetShaderInfoLog(fragment_shader, 512, info_log_len_ptr, info_log.as_mut_ptr() as *mut GLchar);
+            info_log.set_len(*info_log_len_ptr as usize);
+            println!("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n{}", str::from_utf8(&info_log).unwrap());
+        }
+
+        // link shaders
+        let shader_program = gl::CreateProgram();
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
+
+        // check for linking errors
+        gl::GetProgramiv(shader_program, gl::LINK_STATUS, &mut success);
+        if success != gl::TRUE as GLint {
+            gl::GetShaderInfoLog(shader_program, 512, info_log_len_ptr, info_log.as_mut_ptr() as *mut GLchar);
+            info_log.set_len(*info_log_len_ptr as usize);
+            println!("ERROR::SHADER::PROGRAM::LINKING_FAILED\n{}", str::from_utf8(&info_log).unwrap());
+        }
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
+
+        // set up vertex data
+        let vertices: [f32; 9] = [
+            -0.5, -0.5, 0.0,
+            0.5, -0.5, 0.0,
+            0.0, 0.5, 0.0
+        ];
+
+        let (mut vbo, mut vao) = (0, 0);
+        gl::GenVertexArrays(1, &mut vao);
+        gl::GenBuffers(1, &mut vbo);
+        // bind the Vertex Array Object first, then bind and set vertex buffer(s)
+        // and then configure vertex attributes
+        gl::BindVertexArray(vao);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr, 
+            &vertices[0] as *const f32 as *const c_void, 
+            gl::STATIC_DRAW
+        );
+
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * mem::size_of::<GLfloat>() as GLsizei, ptr::null());
+        gl::EnableVertexAttribArray(0);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+
+        gl::BindVertexArray(0);
+
+        (shader_program, vao, vbo)
+    };
+
+    // render loop
+    while !window.should_close() {
+        // events
+        process_events(&mut window, &events);
+
+        //render
+        unsafe { 
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            // draw triangle
+            gl::UseProgram(shader_program);
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+
+        window.swap_buffers();
+        glfw.poll_events();
+    }
+
+    unsafe {
+        gl::DeleteVertexArrays(1, &vao);
+        gl::DeleteBuffers(1, &vbo);
+        gl::DeleteProgram(shader_program);
+    };
+}
+
+fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => {
+                unsafe { gl::Viewport(0, 0, width, height) }
+            }
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => window.set_should_close(true),
+            _ => {}
+        }
+    }
 }
